67796fd2a9c672eaed70b2845800e9fff76b77b9
SoniaGubs <blacksonia@mail.ru>
2023-02-24 16:01:30 +0300

    change

--- src/main/java/com/academy/BookServlet.java
+++ src/main/java/com/academy/BookServlet.java
@@ -1,6 +1,5 @@
 package com.academy;
 
-import com.academy.model.entity.User;
 import com.academy.service.BookService;
 
 import javax.servlet.ServletException;
@@ -9,7 +8,6 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
-import java.io.PrintWriter;
 
 @WebServlet(urlPatterns = "/books")
 public class BookServlet extends HttpServlet {
